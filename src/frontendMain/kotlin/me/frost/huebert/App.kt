5d68769509d1034144dc7d690572d2fe34294c48
Swantje <swantje-t@web.de>
2022-01-29 22:17:42 +0100

    use NavBar

--- src/frontendMain/kotlin/me/frost/huebert/App.kt
+++ src/frontendMain/kotlin/me/frost/huebert/App.kt
@@ -1,11 +1,12 @@
 package me.frost.huebert
 
 import io.kvision.*
-import io.kvision.html.Align
 import io.kvision.html.Div
-import io.kvision.html.h1
+import io.kvision.navbar.NavbarType
+import io.kvision.navbar.nav
+import io.kvision.navbar.navLink
+import io.kvision.navbar.navbar
 import io.kvision.panel.root
-import io.kvision.panel.tabPanel
 
 class App : Application() {
 
@@ -13,25 +14,28 @@ class App : Application() {
         Model.callLights()
         Model.callZones()
 
+        val root = root("kvapp")
+        val lights = Div { lightTable(Model.lights) }
+        val zones = Div { zoneTable(Model.zones) }
 
-        root("kvapp") {
-            h1 {
-                content = "Huebert"
-                align = Align.CENTER
-            }
-            tabPanel {
-                addTab(
-                    title = "Lights",
-                    panel = Div { lightTable(Model.lights) }
-                )
-                addTab(
-                    title = "Zones",
-                    panel = Div { zoneTable(Model.zones) }
-                )
+        root.navbar(label = "Huebert", type = NavbarType.FIXEDTOP) {
+            nav {
+                navLink(label = "Lights") {
+                    onClick {
+                        root.add(lights)
+                        root.remove(zones)
+                    }
+                }
+                navLink(label = "Zones") {
+                    onClick {
+                        root.add(zones)
+                        root.remove(lights)
+                    }
+                }
             }
         }
+        root.add(lights)
     }
-
 }
 
 fun main() {
